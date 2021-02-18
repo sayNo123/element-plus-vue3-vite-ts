8c9cf826d407b7c7a02f15aea6bdae5dee71fd0f
xinuo <vax_sayno@163.com>
2021-02-18 17:35:50 +0800

    main

--- src/App.vue
+++ src/App.vue
@@ -2,17 +2,20 @@
   <div>
     <img alt="Vue logo" src="./assets/logo.png" />
     <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
+    <Demo />
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
 import HelloWorld from './components/HelloWorld.vue'
+import Demo from './components/demo.vue'
 
 export default defineComponent({
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    Demo
   }
 })
 </script>
